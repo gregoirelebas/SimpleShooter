cb8394981b379336b78d3cd5eb203e908075054c
Grégoire Lebas <gregoire.lebas@zerogamesstudios.com>
2025-01-14 10:46:44 +0100

    Move AI using behavior tree instead of code.

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -10,15 +10,17 @@ void AShooterAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-
 	if (AIBehavior != nullptr)
 	{
 		RunBehaviorTree(AIBehavior);
+
 		Blackboard = GetBlackboardComponent();
+		PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
 		if (PlayerPawn && Blackboard != nullptr)
 		{
 			Blackboard->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+			Blackboard->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
 		}
 	}
 }
@@ -27,17 +29,17 @@ void AShooterAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (PlayerPawn != nullptr)
-	{
-		if (LineOfSightTo(PlayerPawn))
-		{
-			MoveToActor(PlayerPawn, AcceptanceRadius);
-			SetFocus(PlayerPawn);
-		}
-		else
-		{
-			StopMovement();
-			ClearFocus(EAIFocusPriority::Default);
-		}
-	}
+	//if (PlayerPawn != nullptr)
+	//{
+	//	if (LineOfSightTo(PlayerPawn))
+	//	{
+	//		MoveToActor(PlayerPawn, AcceptanceRadius);
+	//		SetFocus(PlayerPawn);
+	//	}
+	//	else
+	//	{
+	//		StopMovement();
+	//		ClearFocus(EAIFocusPriority::Default);
+	//	}
+	//}
 }
